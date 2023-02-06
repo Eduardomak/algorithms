3e7216525b034745c01e6200033e6d9938a602b7
Eduardomak <eduardomak92@gmail.com>
2023-02-06 18:29:32 -0300

    feat: adding duplicate challenge

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,12 @@
-def find_duplicate(nums):
+def find_duplicate(nums: list):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(nums, list) or len(nums) <= 1:
+        return False
+    nums.sort()
+    for index in range(1, len(nums)):
+        if type(nums[index]) != int or nums[index] < 1:
+            return False
+        if nums[index] == nums[index - 1]:
+            return nums[index]
+    return False
+    """ raise NotImplementedError """

f18acd7d233b2ec8296ab03ee8a04a3da5a0c680
Eduardomak <eduardomak92@gmail.com>
2023-02-06 18:27:29 -0300

    feat: adding study_schedule challenge

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
-def study_schedule(permanence_period, target_time):
+def study_schedule(permanence_period: list, target_time: int) -> int:
     """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+    try:
+        for _ in permanence_period:
+            if target_time >= _[0] and target_time <= _[1]:
+                count += 1
+    except TypeError:
+        return None
+    return count
